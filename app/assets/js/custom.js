5dced0555e4396110cc41efb800d2c9356a400dd
miroslavribac <miroslavribac@gmail.com>
2017-12-13 15:10:10 +0100

    Update js

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -4,42 +4,57 @@ $(document).ready(function(){
         startDate: '-3d'
     });
 
-    $(".car-registration .next").click(function(){
-        $(".booking-date").toggleClass("active");
-        $(".car-registration").toggleClass("hidden");
-    });
+    toggleBookingFormInputs();
+    sendBookingData();
 
-    $(".booking-date .next").click(function(){
-        $(".booking-date").toggleClass("hidden");
-        $(".personal-info").toggleClass("active");
-    });
-    $(".personal-info .next").click(function(){
-        $(".personal-info").toggleClass("active");
-        $(".location-info").toggleClass("active");
-    });
-    $(".location-info .your-location").click(function(){
-        $(".location-info .custom-location").toggleClass("active-inline");
-    });
+    function toggleBookingFormInputs(){
 
-    $(".booking-form").submit(function(e){
-        e.preventDefault();
-        var serialized_data = $(this).serialize();
-        $.ajax({
-            url: "classes/AjaxFunctions.php",
-            method: "POST",
-            data: serialized_data,
-            success: function(data){
-                $(".location-info").toggleClass("active");
-                $(".car-registration").toggleClass("hidden");
-                $( '.booking-form' ).each(function(){
-                    this.reset();
-                });
-            },
-            error: function(xhr, status, error){
-                console.log(error);
-            }
+        $(".car-registration .next").click(function(){
+            $(".booking-date").toggleClass("active");
+            $(".car-registration").toggleClass("hidden");
         });
-    });
+        $(".booking-date .next").click(function(){
+            $(".booking-date").toggleClass("hidden");
+            $(".personal-info").toggleClass("active");
+        });
+        $(".personal-info .next").click(function(){
+            $(".personal-info").toggleClass("active");
+            $(".location-info").toggleClass("active");
+        });
+        $(".location-info .your-location").click(function(){
+            $(".location-info .custom-location").toggleClass("active-inline");
+        });
+    }
+
+    function sendBookingData(){
+        $(".booking-form").submit(function(e){
+            e.preventDefault();
+            var serialized_data = $(this).serialize();
+            $.ajax({
+                url: "classes/AjaxFunctions.php",
+                method: "POST",
+                data: serialized_data,
+                success: function(data){
+                    $(".location-info").toggleClass("active");
+                    $(".car-registration").toggleClass("hidden");
+                    $( '.booking-form' ).each(function(){
+                        this.reset();
+                    });
+                },
+                error: function(xhr, status, error){
+                    console.log(error);
+                }
+            });
+        });
+    }
+
+
+
+
+
+
+
+
 
 });
 
